70e4181045166712f99e97d6f54b74477bda723d
HamzaMajeed86 <rahamza146@gmail.com>
2025-01-29 17:34:16 -0800

    completing admin panel blogs, edit blog, delete blog, create blog routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
@@ -8,10 +8,72 @@ import Services from "./pages/Services";
 import AdminNavbar from "./adminDashboard/components/AdminNavbar";
 import AdminFooter from "./adminDashboard/components/AdminFooter";
 import AdminHome from "./adminDashboard/pages/AdminHome";
-import AdminBlogs from "./adminDashboard/pages/AdminBlogs";
+import EditBlog from "./adminDashboard/pages/EditBlog";
 import AddBlog from "./adminDashboard/pages/AddBlog";
+import AdminUsers from "./adminDashboard/pages/AdminUsers";
 
 function App() {
+  const [blogs, setBlogs] = useState([
+    {
+      id: 1,
+      image: "https://img.freepik.com/free-photo/online-blog_53876-123696.jpg",
+      title: "First Blog Post",
+      content:
+        "This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.",
+      author: "jon",
+    },
+    {
+      id: 2,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9NxVa2mbygkogxqj0VCfaBmAYTj6ZBCsiEA&s",
+      title: "Second Blog Post",
+      content:
+        "This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.",
+      author: "bob",
+    },
+    {
+      id: 3,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_QT9kvgpcVvrp_SX0RQbfjtDv0pbq1gOKLw&s",
+      title: "Third Blog Post",
+      content:
+        "This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.",
+      author: "stev",
+    },
+    {
+      id: 4,
+      image: "https://img.freepik.com/free-photo/online-blog_53876-123696.jpg",
+      title: "Four Blog Post",
+      content:
+        "This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.",
+      author: "peter",
+    },
+    {
+      id: 5,
+      image: "https://img.freepik.com/free-photo/online-blog_53876-123696.jpg",
+      title: "Four Blog Post",
+      content:
+        "This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.",
+      author: "peter",
+    },
+    {
+      id: 6,
+      image: "https://img.freepik.com/free-photo/online-blog_53876-123696.jpg",
+      title: "Four Blog Post",
+      content:
+        "This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.",
+      author: "peter",
+    },
+    {
+      id: 7,
+      image: "https://img.freepik.com/free-photo/online-blog_53876-123696.jpg",
+      title: "Four Blog Post",
+      content:
+        "This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post. This is the content of the first blog post.",
+      author: "peter",
+    },
+  ]);
+
   const MainLayout = () => {
     return (
       <>
@@ -42,9 +104,19 @@ function App() {
     {
       element: <AdminLayout />,
       children: [
-        { path: "/admin", element: <AdminHome /> },
-        { path: "/admin/blogs", element: <AdminBlogs /> },
-        { path: "/admin/add-blog", element: <AddBlog /> },
+        {
+          path: "/admin",
+          element: <AdminHome blogs={blogs} setBlogs={setBlogs} />,
+        },
+        { path: "/admin/users", element: <AdminUsers /> },
+        {
+          path: "/admin/blogs/edit/:id",
+          element: <EditBlog blogs={blogs} setBlogs={setBlogs} />,
+        },
+        {
+          path: "/admin/add-blog",
+          element: <AddBlog blogs={blogs} setBlogs={setBlogs} />,
+        },
       ],
     },
   ]);
